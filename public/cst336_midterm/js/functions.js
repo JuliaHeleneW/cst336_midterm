1eeed74001fed9aed5b058f2e5381c735c7e8753
EC2 Default User <ec2-user@ip-172-31-38-30.us-east-2.compute.internal>
2020-03-05 03:39:26 +0000

    Not working commit

---
+++ public/cst336_midterm/js/functions.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+   $("#queryButton").on("click",findBook);
+   function findBook(){
+       /*$("#cover-img").html("");
+          $("#title").html("");
+          $("#author").html("");
+          $("#publish-year").html("");
+          $("#publisher").html("");
+          $("#isbn-print").html("");
+          $("#pages").html("");*/
+          var isbn=$("#isbn").val();
+       //generate an AJAX call
+       $.ajax({
+        url: "https://openlibrary.org/api/books?bibkeys=ISBN:"+isbn+"&format=json&jscmd=data",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            var cover=data["ISBN:"+isbn]["cover"]["small"];
+          //put the right data in the right elements
+          $("#cover-img").append('<img src="'+cover+'">');
+          $("#title").append('Title: '+data["ISBN:"+isbn]["title"]);
+          $("#author").append('Author: '+data["ISBN:"+isbn]["authors"][0]);
+          $("#publish-year").append('Publish: '+data["ISBN:"+isbn]["publish_date"]);
+          $("#publisher").append('Publisher: '+data["ISBN:"+isbn]["publishers"][0]);
+          $("#isbn-print").append('ISBN: '+isbn);
+          $("#pages").append('Pages: '+data["ISBN:"+isbn]["number_of_pages"]);
+        },
+        error:function(error){
+            alert(error)
+        },
+      });
+   }
+});
